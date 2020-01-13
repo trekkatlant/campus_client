e17453cff550e0b5bf373f46df0e2323a334dd47
Anthony <anthony@Apples-MacBook-Pro.local>
2020-01-13 13:16:54 -0500

    added styling for allcampus

--- src/components/allCampuses.js
+++ src/components/allCampuses.js
@@ -10,7 +10,10 @@ class AllCampuses extends Component{
     let campuses = [{name:"Harvard",imageUrl:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ff.hypotheses.org%2Fwp-content%2Fblogs.dir%2F1204%2Ffiles%2F2013%2F04%2Fhunter-college-logo.png&f=1&nofb=1"}
                     ,{name:"Hunter",imageUrl:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ff.hypotheses.org%2Fwp-content%2Fblogs.dir%2F1204%2Ffiles%2F2013%2F04%2Fhunter-college-logo.png&f=1&nofb=1"}
                     ,{name:"Baruch",imageUrl:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ff.hypotheses.org%2Fwp-content%2Fblogs.dir%2F1204%2Ffiles%2F2013%2F04%2Fhunter-college-logo.png&f=1&nofb=1"}];
-    return <div className="AllCampuses">
+    return(
+          <div>
+            <h1> All Campuses </h1>
+          <div className="AllCampuses">
               {campuses.length > 0 ?
                 campuses.map((campus,index) => {
                   return <CampusCard name={campus.name} imageUrl={campus.imageUrl}/>
@@ -19,7 +22,8 @@ class AllCampuses extends Component{
                 <p>No Campuses</p>
               }
            </div>
-
+           </div>
+         )
   }
 }
 
