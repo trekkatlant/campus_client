128bc1c5128b0c50329ac109b52a8c932cd75fc9
Anthony <anthony@Apples-MacBook-Pro.local>
2020-01-14 10:41:08 -0500

    added basic all student display

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import AllCampuses from './components/allCampuses';
-import Campus from './components/singleCampus'
+import Campus from './components/singleCampus';
+import AllStudents from './components/allStudents';
 
 class App extends Component {
   render() {
     const AllCampusesComponent = () => <AllCampuses/>
+    const AllStudentsComponent = () => <AllStudents/>
     const HomePageComponent = () => <Home/>
     const SinglePageComponent = () => <Campus/>
     return (
@@ -16,6 +18,7 @@ class App extends Component {
         <Route exact path="/" render= {HomePageComponent} />
         <Route exact path="/campuses" render ={AllCampusesComponent}/>
         <Route exact path="/campus" render={SinglePageComponent}/>
+        <Route exact path="/students" render={AllStudentsComponent}/>
       </Switch>
     </Router>
     )
