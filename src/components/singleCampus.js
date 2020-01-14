9266ef20097ea613e976f13225a1a4a027faeea4
Anthony <anthony@Apples-MacBook-Pro.local>
2020-01-14 11:02:03 -0500

    added students to campus page

--- src/components/singleCampus.js
+++ src/components/singleCampus.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import StudentCard from './StudentCard'
+import '../styles/singleCampus.css'
+import Navbar from './navbar'
 
 class Campus extends Component{
   constructor(props){
@@ -9,7 +12,11 @@ class Campus extends Component{
     let address = "695 Park Ave"
     let imageUrl = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ff.hypotheses.org%2Fwp-content%2Fblogs.dir%2F1204%2Ffiles%2F2013%2F04%2Fhunter-college-logo.png&f=1&nofb=1"
     let text = "Public university in NYC"
+    let students = [{name:"Jack",campus:"Hunter",imageUrl:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ff.hypotheses.org%2Fwp-content%2Fblogs.dir%2F1204%2Ffiles%2F2013%2F04%2Fhunter-college-logo.png&f=1&nofb=1'},
+                    {name:"John",campus:"Baruch",imageUrl:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ff.hypotheses.org%2Fwp-content%2Fblogs.dir%2F1204%2Ffiles%2F2013%2F04%2Fhunter-college-logo.png&f=1&nofb=1'},
+                    {name:"Adam",campus:"Hunter",imageUrl:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ff.hypotheses.org%2Fwp-content%2Fblogs.dir%2F1204%2Ffiles%2F2013%2F04%2Fhunter-college-logo.png&f=1&nofb=1'}]
     return ( <div>
+                <Navbar/>
                 <div className="campusData">
                     <p> {name} </p>
                     <p> {address} </p>
@@ -17,8 +24,15 @@ class Campus extends Component{
                     <button> Edit </button>
                     <button> Delete </button>
                 </div>
+                <h1> Students </h1>
                 <div className="students">
-                    <p> Students </p>
+                  {students.length > 0 ?
+                    students.map((student,index) => {
+                      return <StudentCard name={student.name} campus={student.campus} imageUrl={student.imageUrl}/>
+                    })
+                    :
+                    <p>No Students</p>
+                  }
                 </div>
              </div>
     )
